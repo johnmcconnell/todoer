97cb6b84a9055df041ce75ebb466b716b1a82b46
John McConnell <john_mcconnell@apple.com>
2017-12-04 21:28:34 -0800

    add a view

--- todoer/src/cljs/todoer/views.cljs
+++ todoer/src/cljs/todoer/views.cljs
@@ -3,6 +3,28 @@
             [todoer.subs :as subs]
             ))
 
+(defn navbar-panel
+  []
+  (let [name (re-frame/subscribe [:name])]
+  [:nav.navbar.navbar-toggleable-md.navbar-inverse.bg-inverse.fixed-top
+   {:key "navigation-bar"}
+   [:button.navbar-toggler.navbar-toggler-right
+    {:type "button"
+     :data-toggle "collapse"
+     :data-target "#navbarsExampleDefault"
+     :aria-controls "navbarsExampleDefault"
+     :aria-expanded "false"
+     :aria-label "Toggle navigation"}
+    [:span {:class "navbar-toggler-icon"}]]
+   [:a {:class "navbar-brand", :href "/"} @name]])
+
+(defn main-panel []
+    (fn []
+      [:div
+       (list
+         (navbar-panel name)
+         [:div.container-fluid {:key "heading"}
+          [:h1 "Hello from " @name]])])))
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div "Hello from " @name]))
